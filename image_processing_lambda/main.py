0e7a6510fad397f7f915b7b2733e896b6c07ac2a
jeffoowz <jeffoowz@gmail.com>
2026-04-07 13:40:36 +0800

    add template code

--- image_processing_lambda/main.py
+++ image_processing_lambda/main.py
@@ -1,5 +1,16 @@
 import json
+from aws_lambda_powertools.utilities.batch import BatchProcessor, EventType, process_partial_response
+from aws_lambda_powertools.utilities.data_classes import S3Event
+
+
+processor = BatchProcessor(event_type=EventType.SQS)
+
+
+def record_handler(record):
+    payload: S3Event = json.loads(record["body"])
+    print(f"Processing record with payload: {payload}")
 
 
 def lambda_handler(event, context):
-    return json.dumps({"message": "Hello from Lambda!"})
+    return process_partial_response(
+        event=event, record_handler=record_handler, processor=processor, context=context)
