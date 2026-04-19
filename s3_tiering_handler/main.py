78a8830a5d1aec9c4e2ef360a96c0ee475011427
jeffoowz <jeffoowz@gmail.com>
2026-04-19 18:12:20 +0800

    fix s3 tier lambda logic

--- s3_tiering_handler/main.py
+++ s3_tiering_handler/main.py
@@ -13,7 +13,7 @@ from config import S3_BUCKET_GLACIER_FLEXIBLE_RETRIEVAL, S3_BUCKET_GLACIER_INSTA
 def lambda_handler(event, context):
     # handle glacier ir -> glacier
     
-    to_glacier = opensearch_service.search_image_with_infrequenty_access("GLACIER", 30)
+    # to_glacier = opensearch_service.search_image_with_infrequenty_access("GLACIER", 30)
     for docs in to_glacier:
         s3_file_name = docs["s3_file_path"]
         s3_service.move_file(docs["s3_bucket_name"], s3_file_name, S3_BUCKET_GLACIER_FLEXIBLE_RETRIEVAL, s3_file_name, "GLACIER")
@@ -32,6 +32,5 @@ def lambda_handler(event, context):
     to_standard_ia = opensearch_service.search_image_with_infrequenty_access("STANDARD_IA", 5)
     for docs in to_standard_ia:
         s3_file_name = docs["s3_file_path"]
-        s3_service.move_file(docs["s3_bucket_name"], s3_file_name, S3_BUCKET_STANDARD_IA, s3_file_name, "STANDARD", delete_source_file=False)
-        dynamodb_service.update_ddb_record(docs['PK'], docs["SK"], storage_class="STANDARD", s3_bucket_name=S3_BUCKET_STANDARD_IA)
-
+        s3_service.move_file(docs["s3_bucket_name"], s3_file_name, S3_BUCKET_STANDARD_IA, s3_file_name, "STANDARD_IA")
+        dynamodb_service.update_ddb_record(docs['PK'], docs["SK"], storage_class="STANDARD_IA", s3_bucket_name=S3_BUCKET_STANDARD_IA)
--- s3_tiering_handler/service/dynamodb_service.py
+++ s3_tiering_handler/service/dynamodb_service.py
@@ -2,11 +2,10 @@ import boto3
 from config import DDB_TABLE
 from boto3.dynamodb.conditions import Key, Attr
 
-dynamodb_client = boto3.resource("dynamodb", region_name="us-east-1")
+dynamodb_client = boto3.resource("dynamodb")
 table = dynamodb_client.Table(DDB_TABLE)
 
 def update_ddb_record(pk: str, sk: str, storage_class: str, s3_bucket_name):
-  return
   update_result = table.update_item(
     Key={"PK": pk, "SK": sk},
     UpdateExpression="set lastest_s3_storage_tier = :lastest_s3_storage_tier, s3_bucket_name = :s3_bucket_name",
